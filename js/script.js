97d733c1de60df47560674cadcef61eecc8a8169
Iliev Artem <aartemiliev@gmail.com>
2024-09-16 16:47:27 +0300

    Studyed cycles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+"use strict"
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++){
+
+    for (let j = 0; j < i; j++){
+        result += "*";
+    }
+
+    result += '\n';
+}
+
+console.log(result);
+
+// first - мітка
+first: for(let i = 0; i < 3; i++){
+    console.log(`First level: ${i}`);
+    for(let j = 0; j < 3; j++){
+        console.log(`Second level: ${j}`);
+        for(let k = 0; k < 5; k++){
+            if(k === 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
+
 const numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
 console.log(numberOfFilms);
 
@@ -15,7 +43,7 @@ const a = prompt('Один з переглянутих останнії філь
       c = prompt('Один з переглянутих останнії фільмів?', ''),
       d = +prompt('На скільки його оціните?', '');
 
-personalMovieDB[movies[a] = b];
+personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
